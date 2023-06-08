57ff83cffa76529f8a86d22f31d0fd662e08cfa1
Valeriy Sidorenko <balepas@yandex-team.ru>
2023-06-08 11:58:26 +0200

    fix(Calendar): add translations

--- src/components/Calendar/i18n/index.ts
+++ src/components/Calendar/i18n/index.ts
@@ -1,9 +1,8 @@
-// @ts-expect-error
-import {registerKeyset} from '@gravity-ui/uikit/i18n';
+import {addComponentKeysets} from '@gravity-ui/uikit/i18n';
 
 import {NAMESPACE} from '../../../utils/cn';
 
 import en from './en.json';
 import ru from './ru.json';
 
-export const i18n = registerKeyset({en, ru}, `${NAMESPACE}calendar`);
+export const i18n = addComponentKeysets({en, ru}, `${NAMESPACE}calendar`);
