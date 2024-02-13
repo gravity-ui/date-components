2a2838938755ecd2a8db5d0de999d805a8dac897
Valerii Sidorenko <balepas@yandex-team.ru>
2024-02-13 20:13:00 +0100

    fix: accept events handlers with custom targets (#45)

--- src/components/types/events.ts
+++ src/components/types/events.ts
@@ -1,15 +1,15 @@
 export interface KeyboardEvents {
     /** Handler that is called when a key is pressed. */
-    onKeyDown?: (e: React.KeyboardEvent) => void;
+    onKeyDown?(e: React.KeyboardEvent): void;
     /** Handler that is called when a key is released. */
-    onKeyUp?: (e: React.KeyboardEvent) => void;
+    onKeyUp?(e: React.KeyboardEvent): void;
 }
 
 export interface FocusEvents<Target = Element> {
     /** Handler that is called when the element receives focus. */
-    onFocus?: (e: React.FocusEvent<Target>) => void;
+    onFocus?(e: React.FocusEvent<Target>): void;
     /** Handler that is called when the element loses focus. */
-    onBlur?: (e: React.FocusEvent<Target>) => void;
+    onBlur?(e: React.FocusEvent<Target>): void;
 }
 
 export interface FocusableProps<Target = Element> extends FocusEvents<Target> {
