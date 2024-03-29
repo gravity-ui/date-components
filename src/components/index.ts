4b84a1659119c14d02a9d551562bb5e58c634c92
Evgeny Alaev <alaev89@yandex-team.ru>
2024-03-29 16:34:52 +0300

    fix(RangeDatePicker): add component export

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,6 +2,7 @@ export * from './Calendar';
 export * from './RangeCalendar';
 export * from './DateField';
 export * from './DatePicker';
+export * from './RangeDatePicker';
 export * from './RelativeDateField';
 export * from './RelativeDatePicker';
 export * from './RangeDateField';
