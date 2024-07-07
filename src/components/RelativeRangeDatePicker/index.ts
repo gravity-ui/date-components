95b9b4deace290d09e34a55dea752355df24443c
geosos <42944548+saxumcordis@users.noreply.github.com>
2024-07-07 13:31:42 +0300

    feat(RelativeRangeDatePicker): export default presets (#108)

--- src/components/RelativeRangeDatePicker/index.ts
+++ src/components/RelativeRangeDatePicker/index.ts
@@ -2,3 +2,11 @@ export * from './RelativeRangeDatePicker';
 export {PickerDialog as RelativeRangeDatePickerDialog} from './components/PickerDialog/PickerDialog';
 export type {PickerDialogProps as RelativeRangeDatePickerDialogProps} from './components/PickerDialog/PickerDialog';
 export * from './hooks/useRelativeRangeDatePickerState';
+export {
+    DEFAULT_DATE_PRESETS,
+    DEFAULT_TIME_PRESETS,
+    DEFAULT_OTHERS_PRESETS,
+} from './components/Presets/defaultPresets';
+export {getDefaultPresetTabs} from './components/Presets/utils';
+export type {Preset as RelativeRangeDatePickerPreset} from './components/Presets/defaultPresets';
+export type {PresetTab as RelativeRangeDatePickerPresetTab} from './components/Presets/utils';
