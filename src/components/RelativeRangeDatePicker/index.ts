071b49ae4f85d818283282cf16353c5a77ff9790
Anastasia Zamkovskaya <trabemz@yandex.ru>
2024-09-20 15:04:00 +0200

    fix(RelativeRangeDatePicker): export RelativeRangeDatePickerProps

--- src/components/RelativeRangeDatePicker/index.ts
+++ src/components/RelativeRangeDatePicker/index.ts
@@ -1,4 +1,5 @@
 export * from './RelativeRangeDatePicker';
+export * from './types';
 export {PickerDialog as RelativeRangeDatePickerDialog} from './components/PickerDialog/PickerDialog';
 export type {PickerDialogProps as RelativeRangeDatePickerDialogProps} from './components/PickerDialog/PickerDialog';
 export * from './hooks/useRelativeRangeDatePickerState';
