1383178cfad3175853619edbfef00175e9fa7938
ddgrishkin <gddbox@mail.ru>
2025-10-30 23:14:39 +0300

    feat: cleanup playwright config from useless comments

--- playwright/playwright.config.ts
+++ playwright/playwright.config.ts
@@ -27,36 +27,25 @@ reporter.push(
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-    /* Look for test files in the "tests" directory, relative to this configuration file. */
     testDir: pathFromRoot('src'),
-    /* Glob patterns or regular expressions that match test files. For example, '*todo-tests/*.spec.ts'. By default, Playwright runs .*(test|spec).(js|ts|mjs) files. */
     testMatch: '**/__tests__/*.visual.test.tsx',
     updateSnapshots: process.env.UPDATE_REQUEST ? 'all' : 'missing',
     snapshotPathTemplate:
         '{testDir}/{testFileDir}/../__snapshots__/{testFileName}-snapshots/{arg}{-projectName}-linux{ext}',
-    /* Maximum time one test can run for. */
     timeout: 10 * 1000,
-    /* Run tests in files in parallel */
     fullyParallel: true,
-    /* Fail the build on CI if you accidentally left test.only in the source code. */
     forbidOnly: Boolean(process.env.CI),
-    /* Retry on CI only */
     retries: process.env.CI ? 1 : 0,
-    /* Opt out of parallel tests on CI. */
     workers: process.env.CI ? 1 : undefined,
-    /* Reporter to use. See https://playwright.dev/docs/test-reporters */
     reporter,
-    /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
         testIdAttribute: 'data-qa',
-        /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
         trace: 'on',
         headless: true,
         screenshot: 'only-on-failure',
         timezoneId: 'UTC',
         ctCacheDir: process.env.IS_DOCKER ? '.cache-docker' : '.cache',
     },
-    /* Configure projects for major browsers */
     projects: [
         {
             name: 'chromium',
