9ffee377ded7e4bf4361442653120f1113ef26a9
ddgrishkin <gddbox@mail.ru>
2025-10-30 22:47:06 +0300

    feat: setup playwright

---
+++ scripts/playwright-docker.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+SCRIPTS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+IMAGE_NAME="mcr.microsoft.com/playwright"
+IMAGE_TAG="v$($SCRIPTS_DIR/playwright-version.sh)-jammy"
+
+NODE_MODULES_CACHE_DIR="$HOME/.cache/uikit-playwright-docker-node-modules"
+
+command_exists() {
+  command -v "$*" >/dev/null 2>&1
+}
+
+run_command() {
+  $CONTAINER_TOOL run --rm --network host -it -w /work \
+    -v $(pwd):/work \
+    -v "$NODE_MODULES_CACHE_DIR:/work/node_modules" \
+    -e IS_DOCKER=1 \
+    "$IMAGE_NAME:$IMAGE_TAG" \
+    /bin/bash -c "$*"
+}
+
+if command_exists docker; then
+  CONTAINER_TOOL="docker"
+elif command_exists podman; then
+  CONTAINER_TOOL="podman"
+else
+  echo "Neither Docker nor Podman is installed on the system."
+  exit 1
+fi
+
+if [[ "$*" = "clear-cache" ]]; then
+  rm -rf "$NODE_MODULES_CACHE_DIR"
+  rm -rf "./playwright/.cache-docker"
+  exit 0
+fi
+
+if [[ ! -d "$NODE_MODULES_CACHE_DIR" ]]; then
+  mkdir -p "$NODE_MODULES_CACHE_DIR"
+  run_command 'npm ci'
+fi
+
+run_command "$*"
